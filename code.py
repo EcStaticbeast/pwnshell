b2e04ea79565884f7cfc572a70f6f2bd3d2416f3
EcStaticbeast <130395352+EcStaticbeast@users.noreply.github.com>
2023-04-21 14:49:32 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,34 +1,42 @@
-#!/usr/bin/env python3
-
 from pwn import *
-import sys
+#import binascii
 from base64 import b64decode
-import re
-import decimal
-io = remote('43.204.138.14','32369',level="DEBUG"
-data =  io.recvall()
-a,b = data.split(':',1)
-
+from decimal import Decimal
+io = remote('localhost',1337)#,level="DEBUG")
+data =  io.recvlines(2)[1].decode()
+a,b = data.split(': ')
+print(a)
 def rot13(str):
-	return ''.join([chr((ord(letter) - 97 + 13) % 26 + 97) for letter in str.lower()])
- 
-if "base64" in b :
-       v = b64decode(b)
-       print(v)
-	io.sendline(v)
-
-elif "rot13" in b :
-      
-      v=rot13(b)
-      print(v)
+    return ''.join([chr((ord(letter) - 97 + 13) % 26 + 97) for letter in str.lower()])
+if "base64" in a :
+    v = b64decode(b)
+    print("b64")
+    print(v)
     io.sendline(v)
-
-elif  "decimal" in b :
-
-       dec = decimal.Decimal("b")
-       str(dec)
-     io.sendline(v)
-else 
-	print(error)
-
-io.interactive()
+elif "rot_13" in a :
+    print("rot13")
+    v=rot13(b)
+    io.sendline(v)
+    
+elif "decimal" in a :
+    result = 0
+    b = b.split()
+    c = []
+    v = ''
+    print (b)
+    for d in b:
+       integer = int(b[result])
+       c.append(integer)
+       v = chr(c[result])
+#       v.append.append(v)
+#       print(v)
+       print((v),end="")
+#       s.strip()
+#       print (s)
+       result += 1
+#    s.append(v)
+#    print(s)
+    io.sendline(v)#,end="")
+else :
+    print(error)
+print(io.recvline())
