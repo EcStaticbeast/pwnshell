776f148c63e47a5465302e33eecb4fff832a5124
EcStaticbeast <130395352+EcStaticbeast@users.noreply.github.com>
2023-04-22 13:59:55 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,22 +1,31 @@
 from pwn import *
 from base64 import b64decode
 from decimal import Decimal
+
+
 io = remote('localhost',1337)#,level="DEBUG")
 data =  io.recvlines(2)[1].decode()
 a,b = data.split(': ')
 print(a)
+
+
 def rot13(str):
     return ''.join([chr((ord(letter) - 97 + 13) % 26 + 97) for letter in str.lower()])
 s = ''
+
 if "base64" in a :
     v = b64decode(b)
     print("b64")
     print(v)
     io.sendline(v)
+    
+    
 elif "rot_13" in a :
     print("rot13")
     v=rot13(b)
     io.sendline(v)
+    
+    
 elif "decimal" in a :
     result = 0
     b = b.split()
@@ -33,4 +42,30 @@ elif "decimal" in a :
     io.sendline(s)
 else :
     print(error)
-print(io.recvline())
+
+stage2 =  io.recvall()
+# print(stage2)
+e,f,g = stage2.split(b':',(2))
+# print(e)
+# print(f) #cipher
+# print(g) #key
+
+
+def decryption(a,b): 
+        orig_text = [] 
+        for i in range(len(a)): 
+            if i<len(b):
+               x = (ord(a[i]) -ord(b[i]) + 26) % 26
+               x += ord('A')  
+               print (i)
+            else :
+                x = (ord(a[i]) -ord(b[i%len(b)]) + 26) % 26
+                x += ord('A')  
+                print (i)           
+            orig_text.append(chr(x)) 
+        return("" . join(orig_text))    
+        
+f="KOGPC BFVGS"
+g="DORAEMON"
+
+print(decryption(f,g))  
